e3696c4428466fe4f8a195e69594f26738f07c58
Christian Hoffmann <mail@hoffmann-christian.info>
2025-04-07 22:14:28 +0200

    Temporarily revert "Fix for Color fade if Transitions are on with colored words"
    The original commit introduces a bug in minute LED coloring as described
    here:
    https://github.com/ESPWortuhr/Multilayout-ESP-Wordclock/issues/552
    
    This reverts commit f9a42862eb339d38233901d4f74a981beea20bdf.

--- include/Transitiontypes/Transition.hpp
+++ include/Transitiontypes/Transition.hpp
@@ -482,8 +482,8 @@ void Transition::setMinute() {
         }
         for (uint8_t i = 0; i < 4; i++) {
             led.setPixel(minArray[i],
-                         HsbColor{m > i ? foregroundMinute : background});
-            // TODO: fading transition for Minutes
+                         HsbColor{m > i ? foreground : background});
+            // TODO: foregroundMinute
         }
     }
 }
